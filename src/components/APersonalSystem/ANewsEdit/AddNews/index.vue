df95df2bb96713708e1d7f1095c6ce068d7efdd8
asitikovdo <asitikovdo@gmail.com>
2020-10-28 00:04:53 +0900

    Add: img preview

--- src/components/APersonalSystem/ANewsEdit/AddNews/index.vue
+++ src/components/APersonalSystem/ANewsEdit/AddNews/index.vue
@@ -3,21 +3,24 @@ div
     v-dialog(v-model='dialog' persistent='' width='500px')
       v-card(tile='').pa-5
         v-card(outlined='' elevation="19" tile='')
-          v-img.white--text.align-end(height='250px' src='https://www.amurobl.ru/upload/iblock/5e2/image_21_08_20_02_41_5.jpeg')
+          v-img.white--text.align-end(height='250px' :src='defaultImg' v-if="!newPost.img")
+          v-img.white--text.align-end(height='250px' :src='previewImg' v-if="newPost.img")
             v-card-title
               v-text-field(label="Заголовок" dark='' v-model="newPost.title").title
-          v-card-subtitle.pb-0.mt-5
+          v-card-subtitle.pb-0
             | Дата
           v-card-text.text--primary
+            v-divider
             v-textarea(auto-grow='' value='Текст' v-model="newPost.text" autofocus='')
             //v-file-input(label='Вставить изображение' hide-input='' accept='image/*' prepend-icon='mdi-camera' small-chips='' v-model="newPost.img")
           v-divider
           v-card-actions
+            //v-btn(text='' color='deep-purple accent-4' @click="dialog = false")
             v-btn(text='' color='deep-purple accent-4' @click="dialog = false")
               | Отмена
             v-spacer
-            v-file-input(label='Вставить изображение' hide-input='' accept='image/*' prepend-icon='mdi-camera' small-chips='' v-model="newPost.img").mb-3
-            v-btn(text='' color='green darken-2' @click="uploadNews")
+            v-file-input(label='Вставить изображение' @change="imgPreview" hide-input='' prepend-icon='mdi-camera' v-model="newPost.img").mb-3
+            v-btn(text='' color='green darken-3' @click="uploadNews")
               | Сохранить
 </template>
 
@@ -27,6 +30,8 @@ export default {
 name: "AddNews",
   data:()=> ({
     dialog: false,
+    defaultImg: 'https://www.amurobl.ru/upload/iblock/5e2/image_21_08_20_02_41_5.jpeg',
+    previewImg: '',
     newPost: {
       title: '',
       text: 'Текст',
@@ -34,6 +39,12 @@ name: "AddNews",
     },
   }),
   methods: {
+    imgPreview(){
+      if(this.newPost.img) {
+        const file = this.newPost.img;
+        this.previewImg = URL.createObjectURL(file);
+      }
+    },
     showDialog(){
       this.dialog = !this.dialog;
     },
