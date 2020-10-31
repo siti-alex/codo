0d6379a9a575a511bc74e0a8208d3da6e17f4018
asitikovdo <asitikovdo@gmail.com>
2020-10-31 15:12:28 +0900

    Add: full sample events cards

--- src/components/AHomePage/AEvents/index.vue
+++ src/components/AHomePage/AEvents/index.vue
@@ -5,29 +5,30 @@ div
     v-divider
     v-card-title.justify-center.subtitle-2 Ближайшие события
     v-row
-      v-col(v-for='n in 3' cols='4')
+      v-col(v-for='item in cards' cols='4')
         v-card(tile='').mx-5
-          v-img.white--text.align-end(height='200px' src='https://cdn.vuetifyjs.com/images/cards/docks.jpg')
-            v-card-title Открытие ОКЦ
+          v-img.white--text.align-end(height='150px' src='https://cdn.vuetifyjs.com/images/cards/docks.jpg')
+            v-card-title {{item.head}}
           v-card-subtitle.pb-0
           v-chip(color='deep-purple' dark='').ml-3
-            | 2019-09-10
+            | {{item.date}}
           v-card-text.text--primary
-            div На ОКЦ пройдет открытие чего-то
+            div {{item.text}}
     v-divider
     //v-card(width='45%').ml-5
     v-row.ml-3.mr-3
       v-col(cols='6')
-        v-date-picker(v-model='dates' multiple='' :allowed-dates="getAllowedDates" full-width='' locale="ru-ru" color="#8b2639" elevation='10' @input="selectEvent")
+        //v-date-picker(v-model='dates' multiple='' no-title='' :allowed-dates="getAllowedDates" full-width='' locale="ru-ru" color="#8b2639" elevation='10' @input="selectEvent")
+        v-date-picker(v-model='dates' multiple='' no-title='' :allowed-dates="getAllowedDates" full-width='' locale="ru-ru" color="#8b2639" elevation='10' @input="selectEvent")
       v-col(cols='6')
         v-card(tile='' v-if="cardEvent")
           v-img.white--text.align-end(height='278px' src='https://cdn.vuetifyjs.com/images/cards/docks.jpg')
-            v-card-title Открытие ОКЦ
+            v-card-title {{cardEvent.head}}
           v-card-subtitle.pb-0
           v-chip(color='deep-purple' dark='').ml-3
-            | {{date[0]}}
+            | {{cardEvent.date}}
           v-card-text.text--primary
-            div На ОКЦ пройдет открытие чего-то
+            div {{cardEvent.text}}
     br
   br
 </template>
@@ -36,25 +37,52 @@ div
 export default {
 name: "AEvents",
   data: () => ({
-    //date: new Date().toISOString().substr(0, 10),
     date: null,
-    dates: ['2020-10-15', '2020-10-20', '2020-10-25'],
-    curDate: ['2020-10-15', '2020-10-20', '2020-10-25'],
-    cardEvent: false,
+    dates: [],
+    curDate: [],
+    cardEvent: null,
+
+
+    cards: [
+      {
+        head: 'Открытие ОКЦ',
+        text: 'Открытие ОКЦ пройдет в Усть-Ивановке',
+        date: '2020-10-15',
+      },
+      {
+        head: 'Масленица',
+        text: 'Празднование масленицы пройдет на ОКЦ',
+        date: '2020-10-20',
+      },
+      {
+        head: 'Поход в музей',
+        text: 'В 15:00 все идем в музей',
+        date: '2020-10-25',
+      }
+    ]
   }),
   methods: {
-    //allowedDates: val => this.dates.indexOf(val) !== -1,
     getAllowedDates (val) {
       return this.dates.indexOf(val) !== -1;
     },
     selectEvent(){
-      //console.log(this.dates);
-      //console.log(this.curDate.filter(x => !this.dates.includes(x)));
       this.date = this.curDate.filter(x => !this.dates.includes(x));
-      //console.log(this.date);
-      this.dates = ['2020-10-15', '2020-10-20', '2020-10-25'];
-      this.cardEvent = true;
-    }
+      this.cards.forEach(element => {
+        if(element.date === this.date[0]) {
+          this.cardEvent = element;
+        }
+      })
+      this.dates = this.curDate;
+    },
+    sampleDates() {
+      this.cards.forEach(element => {
+        this.dates.push(element.date)
+        this.curDate.push(element.date)
+      });
+    },
+  },
+  created() {
+    this.sampleDates();
   }
 }
 </script>
