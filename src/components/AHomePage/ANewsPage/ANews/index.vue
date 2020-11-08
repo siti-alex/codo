eded902872f9a1b03125739a3ca28c1ce5f8fb4a
asitikovdo <asitikovdo@gmail.com>
2020-11-08 19:32:29 +0900

    Add: full text news design

--- src/components/AHomePage/ANewsPage/ANews/index.vue
+++ src/components/AHomePage/ANewsPage/ANews/index.vue
@@ -3,7 +3,11 @@ div
   v-card.mx-auto(height='100%' width='80%' elevation="6" min-height='700px')
     v-card-title.justify-center Новости
     v-divider
-    div(v-html="text")
+    v-btn(tile='', @click="fGoBack" small='' dark='' color="#8b2639")
+      v-icon mdi-keyboard-backspace
+      | Вернуться
+    v-sheet(width='90%' height='100%' v-html="text").mx-auto.mt-5
+    //div(v-html="text")
 
 
   br
@@ -17,6 +21,9 @@ name: "ANews",
     text: null,
   }),
   methods: {
+    fGoBack(){
+      history.back();
+    },
     getText(){
       Api.getFullTextNews(this.$route.params.id).then(result => {
         console.log(result);
