702ddd2e83907504b6276ca73d68d6d68d3d5773
asitikovdo <asitikovdo@gmail.com>
2020-11-28 22:40:41 +0900

    Add: footer

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,16 @@
       v-spacer
       v-toolbar-title Центр организации довузовского образования при БГПУ
     router-view
+    v-footer(dark='' padless='' style="opacity: 0.9")
+      v-card.white--text.text-center(flat='' tile='' width='100%' color="#8b2639")
+        v-card-text
+          v-btn.mx-4.white--text(v-for='icon in icons' :key='icon' icon='' :href='icon.href')
+            v-icon(size='24px')
+              | {{ icon.icon }}
+        v-divider
+        v-card-text.white--text
+          | {{ new Date().getFullYear() }} —
+          strong  Ситиков Алексей
 </template>
 
 <style lang="scss">
@@ -15,7 +25,25 @@ import AHome from "@/views/AHome/index";
 export default {
   components: {AHome},
   data: () => ({
-
+    // icons: [
+    //   'mdi-vk',
+    //   'mdi-instagram',
+    //   'mdi-school',
+    // ],
+      icons: [
+        {
+          icon: 'mdi-vk',
+          href: null,
+        },
+        {
+          icon: 'mdi-instagram',
+          href: null,
+        },
+        {
+          icon: 'mdi-school',
+          href: 'https://bgpu.ru/func/educational/codo.html',
+        },
+      ],
   }),
   methods: {
     home(){
