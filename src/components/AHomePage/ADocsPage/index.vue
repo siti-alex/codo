22d7682489dda0255abae7c8ab875ca1895a8177
lehajuk <asitikovdo@mail.com>
2020-12-03 13:15:59 +0900

    Add: cms for docs

--- src/components/AHomePage/ADocsPage/index.vue
+++ src/components/AHomePage/ADocsPage/index.vue
@@ -3,13 +3,33 @@ div
   v-card.mx-auto(height='100%' width='80%' elevation="6" min-height='700px')
     v-card-title.justify-center Документы
     v-divider
-    h1 Документы
+    v-card(:loading='loading' v-html='info' min-height='700px' flat='').mx-5
   br
 </template>
 
 <script>
+    import axios from 'axios'
 export default {
-name: "ADocsPage"
+name: "ADocsPage",
+    data () {
+        return {
+            top: false,
+            tab: null,
+            info: null,
+            loading: false,
+        }
+    },
+    mounted() {
+        this.loading = true;
+        axios
+            //.get('https://cms.lit.bgpu.ru/public/get/public/codo-site/exam-courses/')
+            .get('https://cms.lit.bgpu.ru/public/get/public/docs/')
+            .then(response => {
+                //console.log(response);
+                this.info = response.data.htmlCode
+                this.loading = false;
+            })
+    }
 }
 </script>
 
