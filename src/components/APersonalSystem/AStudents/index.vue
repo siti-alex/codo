2c85c346bc89d93fdf3c7ecf448c75cba7f86caa
lehajuk <asitikovdo@mail.com>
2021-02-03 11:23:40 +0900

    fix: students design

--- src/components/APersonalSystem/AStudents/index.vue
+++ src/components/APersonalSystem/AStudents/index.vue
@@ -1,19 +1,29 @@
 <template lang="pug">
 div
-  v-card(v-for='n in 6').mb-5
+  v-card().mb-5
     v-subheader Направление
-    v-expansion-panels(accordion='')
+    v-expansion-panels(accordion='' focusable='')
       v-expansion-panel(v-for='student in students' :key='student.id' cols='12' link='')
         v-expansion-panel-header {{student.fio}}
           template(v-if="student.id%2==0" v-slot:actions='')
             v-icon(color='error')
               | mdi-alert-circle
         v-expansion-panel-content
-          h2.subtitle-2
-            | Предметы
+          h2.subtitle-2.mt-2
+            | Подписаные предметы
           v-chip-group(column='')
             v-chip(outlined='' v-for="tag in tags" :key="tag")
               | {{tag}}
+          v-divider
+          h2.subtitle-2.mt-3  Общая информация
+          v-row
+            v-col
+              p.caption Уплочено за месяц: {{student.balance}} руб.
+            v-col
+              p.caption Родитель: {{student.parentFio}}
+            v-col
+              p.caption Номер телефона: {{student.phoneNumber}}
+
           v-btn(color='primary' fab='' x-small='').float-lg-right
             v-icon mdi-pencil
 </template>
