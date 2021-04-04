2b4f4e1d6afa96544cf1f9e503b6697d3f980745
Алексей <asitikovdo@gmail.com>
2021-04-04 11:47:03 +0900

    fix: ubral nafeg eti courses potomuchto pzdc

--- src/components/APersonalSystem/AStudents/index.vue
+++ src/components/APersonalSystem/AStudents/index.vue
@@ -1,52 +1,46 @@
 <template lang="pug">
-div
-  v-row
-    v-col
-    v-col
-      v-text-field(label='Поиск' prepend-icon='mdi-account-search')
-  v-card(:loading='loading').mb-5
-    v-subheader.subtitle-1 Школьники
-      v-btn(icon='' color="#8b2639" @click="showNewStudentForm").float-right
-        v-icon mdi-plus
-    v-expansion-panels(accordion='' focusable='' v-model="panel")
-      v-expansion-panel(v-for='course in courses' :key='course.id' cols='12' link='')
-        v-expansion-panel-header(@click="getAllStudentsByCourse(course.id)") {{course.name}}
-        v-expansion-panel-content
+  div
+    v-row
+      v-col
+      v-col
+        v-text-field(label='Поиск' prepend-icon='mdi-account-search')
+    v-card(:loading='loading').mb-5
+      v-subheader.subtitle-1 Школьники
+        v-btn(icon='' color="#8b2639" @click="showNewStudentForm").float-right
+          v-icon mdi-plus
+      v-expansion-panels(accordion='' focusable='' v-model="panel")
+        v-expansion-panel(v-for='student in students' :key='student.id' cols='12' link='' v-if="!loading")
+          //v-expansion-panel-header(@click="getDisciplinesByUser(student.id)") {{student.fio}}
+          v-expansion-panel-header(@click="selStudent = student") {{student.fio}}
+            template(v-if="student.debtor" v-slot:actions='')
+              v-icon(color='error')
+                | mdi-alert-circle
+          v-expansion-panel-content
+            div(v-if="loadingStudent")
+              v-subheader
+                | Загрузка данных
+              v-progress-linear(indeterminate='' rounded='')
+            div(v-if="!loadingStudent")
+              h2.subtitle-2.mt-2
+                | Подписаные предметы
+              v-chip-group(column='' style="height: 50px;")
+                v-progress-circular(indeterminate='' color='primary' v-if="!disciplines")
+                v-chip(outlined='' v-for="discipline in disciplines" :key="discipline.id" @click="showDisciplineForm" :color="discipline.colorCode")
+                  | {{discipline.name}}
+              v-divider
+              h2.subtitle-2.mt-3  Общая информация
+              v-row
+                v-col
+                  p.caption Уплачено за месяц: {{student.balance}} руб.
+                v-col
+                  p.caption Родитель: {{student.parentFio}}
+                v-col
+                  p.caption Номер телефона: {{student.phoneNumber}}
 
-          v-expansion-panels(accordion='' focusable='')
-            v-expansion-panel(v-for='student in students' :key='student.id' cols='12' link='' v-if="!loading")
-              //v-expansion-panel-header(@click="getDisciplinesByUser(student.id)") {{student.fio}}
-              //v-expansion-panel-header(@click="selStudent = student") {{student.fio}}
-              v-expansion-panel-header(@click="getDisciplinesByUser(student.id)") {{student.fio}}
-                template(v-if="student.debtor" v-slot:actions='')
-                  v-icon(color='error')
-                    | mdi-alert-circle
-              v-expansion-panel-content
-                div(v-if="loadingStudent")
-                  v-subheader
-                    | Загрузка данных
-                  v-progress-linear(indeterminate='' rounded='')
-                div(v-if="!loadingStudent")
-                  h2.subtitle-2.mt-2
-                    | Подписаные предметы
-                  v-chip-group(column='' style="height: 50px;")
-                    v-progress-circular(indeterminate='' color='primary' v-if="!disciplines")
-                    v-chip(outlined='' v-for="discipline in disciplines" :key="discipline.id" @click="showDisciplineForm" :color="discipline.colorCode")
-                      | {{discipline.name}}
-                  v-divider
-                  h2.subtitle-2.mt-3  Общая информация
-                  v-row
-                    v-col
-                      p.caption Уплачено за месяц: {{student.balance}} руб.
-                    v-col
-                      p.caption Родитель: {{student.parentFio}}
-                    v-col
-                      p.caption Номер телефона: {{student.phoneNumber}}
-
-                  v-btn(color='primary' fab='' x-small='' @click="selStudent = student; selStudent.disciplines = disciplines; showChangeStudentForm()").float-right
-                    v-icon mdi-pencil
-  a-new-student-form(ref="aNewStudentForm" @update="getAllStudents()")
-  a-change-student(ref="aChangeStudentForm" :Student="selStudent" @update="getAllStudents()")
+              v-btn(color='primary' fab='' x-small='' @click="selStudent = student; selStudent.disciplines = disciplines; showChangeStudentForm()").float-right
+                v-icon mdi-pencil
+    a-new-student-form(ref="aNewStudentForm" @update="getAllStudents()")
+    a-change-student(ref="aChangeStudentForm" :Student="selStudent" @update="getAllStudents()")
 </template>
 
 <script>
@@ -55,7 +49,7 @@ import ANewStudentForm from "@/components/APersonalSystem/AStudents/ANewStudentF
 import AChangeStudent from "./AChangeStudent/index";
 
 export default {
-name: "AStudents",
+  name: "AStudents",
   components: {AChangeStudent, ANewStudentForm},
   data: () => ({
     bottom: false,
@@ -64,7 +58,6 @@ name: "AStudents",
     allStudents: [],
     disciplines: null,
     loading: false,
-    courses: [],
 
     slicer: 20,
     panel: true,
@@ -77,13 +70,14 @@ name: "AStudents",
    */
   methods: {
     showDisciplineForm(){
-        alert("Че смотришь, мем только строится еще")
+      alert("Че смотришь, мем только строится еще")
     },
     getAllStudents() {
       this.loading = true;
       Api.getAllStudents().then(value => {
             this.allStudents = value.data;
             this.students = this.allStudents.slice(0,this.slicer);
+            console.log(this.students)
             console.log(this.allStudents)
             this.loading = false;
           },
@@ -92,76 +86,65 @@ name: "AStudents",
             this.loading = false;
           });
     },
-      getAllCourses(){
-        Api.getAllCourses().then(value => {
-          console.log(value.data);
-          this.courses = value.data;
-        })
-      },
-      getAllStudentsByCourse(id){
-        Api.getAllStudentsByCourse(id).then(value => {
-          console.log(id);
-          this.students = value.data;
-        })
-      },
-      getDisciplinesByUser(id) {
-          //this.loadingStudent = true;
-          this.disciplines = null;
-          Api.getDisciplinesByUser(id).then(value => {
-                  this.disciplines = value.data;
-                  console.log(this.disciplines)
-                  // if(this.students.length > 80){
-                  //     setTimeout(() => (this.loadingStudent = false), 1000)
-                  // }
-                  // else {
-                  //     this.loadingStudent = false
-                  // }
-                  //console.log(value)
-              },
-              () => {
-                  console.log('Ошибка');
-                  //this.loadingStudent = false;
-              });
-      },
+    getDisciplinesByUser(id) {
+      //this.loadingStudent = true;
+      this.disciplines = null;
+      Api.getDisciplinesByUser(id).then(value => {
+            this.disciplines = value.data;
+            console.log(this.disciplines)
+            // if(this.students.length > 80){
+            //     setTimeout(() => (this.loadingStudent = false), 1000)
+            // }
+            // else {
+            //     this.loadingStudent = false
+            // }
+            //console.log(value)
+          },
+          () => {
+            console.log('Ошибка');
+            //this.loadingStudent = false;
+          });
+    },
     showNewStudentForm() {
       this.$refs.aNewStudentForm.showDialog();
     },
     showChangeStudentForm() {
-        console.log(this.selStudent);
-          this.$refs.aChangeStudentForm.showDialog();
-      },
-      scrollFunction(){
-          //this.bottom = document.body.scrollTop > 100 || document.documentElement.scrollTop > 100;
-          this.bottom = document.documentElement.getBoundingClientRect().bottom < document.documentElement.clientHeight + 200;
+      console.log(this.selStudent);
+      this.$refs.aChangeStudentForm.showDialog();
+    },
+    scrollFunction(){
+      //this.bottom = document.body.scrollTop > 100 || document.documentElement.scrollTop > 100;
+      this.bottom = document.documentElement.getBoundingClientRect().bottom < document.documentElement.clientHeight + 200;
 
-      },
+    },
   },
   mounted() {
-    //window.onscroll = () => {this.scrollFunction()};
-    //this.getAllStudents();
-    this.getAllCourses();
+    window.onscroll = () => {this.scrollFunction()};
+    this.getAllStudents();
   },
-    watch: {
-        // bottom: {
-        //     handler () {
-        //         if(this.bottom){
-        //             this.slicer += 10;
-        //             this.students = this.allStudents.slice(0,this.slicer);
-        //         }
-        //
-        //     }
-        // },
-        panel: {
-            handler (){
-                if(this.panel !== undefined){
-                    this.getDisciplinesByUser(this.selStudent.id)
-                }
-            }
+  watch: {
+    bottom: {
+      handler () {
+        if(this.bottom){
+          this.slicer += 10;
+          this.students = this.allStudents.slice(0,this.slicer);
         }
+
+      }
+    },
+    panel: {
+      handler (){
+        if(this.panel !== undefined){
+          this.getDisciplinesByUser(this.panel+1)
+          this.getDisciplinesByUser(this.selStudent.id)
+        }
+      }
     }
+  }
 }
 </script>
 
 <style scoped>
 
 </style>
+
