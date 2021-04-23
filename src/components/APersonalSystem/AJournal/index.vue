7fce103bb538368d4a13054561812501b2217a32
lehajuk <asitikovdo@mail.com>
2021-04-23 16:02:19 +0900

    add: date picker

--- src/components/APersonalSystem/AJournal/index.vue
+++ src/components/APersonalSystem/AJournal/index.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
   div
-    h1 Журнал
+    v-row(justify='center').mt-6
+      br
+      h1() Журнал уроков
+      v-btn(@click="test")
+      v-date-picker.mt-4(v-model='date' locale="ru-ru" elevation='10' :allowed-dates='getAllowedDates' full-width='' color='#8b2639').ma-5
+
 </template>
 
 <script>
@@ -9,23 +14,44 @@
         name: "AJournal",
         data:() => ({
             dialog: false,
-            eventId: 0,
-            events: [],
-            serverIp: Api.api,
-            curEvent: {},
+            lessons: [],
+            lessonDates: [],
+            date: new Date().toISOString().substr(0, 10),
+
         }),
         methods: {
+            test(){
+              console.log(this.lessonDates);
+            },
             getAllLessons(){
                 Api.getAllLessons().then(value => {
-                        console.log(value);
+                        console.log(value.data);
+                        this.lessons = value.data;
+                        this.lessons.forEach((element => {
+                            this.lessonDates.push(element.date);
+                        }))
                     },
                     () => {
                         console.log('Ошибка');
                     });
             },
-        },
-        mounted() {
+            //allowedDates: val => parseInt(val.split('-')[2], 10) % 2 === 0,
+            //allowedDates: val => console.log(val),
+            //allowedDates: val => this.lessonDates.indexOf(val) !== -1,
+            getAllowedDates (val) {
+                //console.log(JSON.stringify(val));
+                console.log(val);
+                if (this.lessonDates.indexOf(val) !== -1) {
+                    return true
+                } else {
+                    return false
+                }
+            },
 
+        },
+
+        mounted() {
+          this.getAllLessons();
         }
     }
 </script>
