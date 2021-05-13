98cd3763b749e54b28977deabb9321b331aa9c89
lehajuk <asitikovdo@mail.com>
2021-05-13 14:31:29 +0900

    add: journal disciplines

--- src/components/APersonalSystem/AJournal/index.vue
+++ src/components/APersonalSystem/AJournal/index.vue
@@ -2,9 +2,64 @@
   div
     v-row(justify='center').mt-6
       br
-      h1() Журнал уроков
-      v-btn(@click="test")
-      v-date-picker.mt-4(v-model='date' locale="ru-ru" elevation='10' :allowed-dates='getAllowedDates' full-width='' color='#8b2639').ma-5
+      //v-btn(@click="test") test
+      v-date-picker.mt-4(v-model='date' @input="selectedDay" locale="ru-ru" :allowed-dates='getAllowedDates' full-width='' landscape='' color='#8b2639').ma-5
+      v-divider
+      v-container(v-if="selectDay")
+        v-row()
+          v-col(cols='4')
+            v-card(width='600' flat='')
+                v-card-title
+                  p.ml-3
+                    | {{selectDay.teacher.fio}}
+                v-card-text
+                  .font-weight-bold.ml-8.mb-2
+                    | Предметы
+                  v-timeline(align-top='' dense='')
+                    //v-timeline-item(v-for='discipline in selectDay.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
+                    v-timeline-item(v-for='discipline in selectDay.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
+                      div
+                        div.font-weight-normal
+                          strong {{ selectDay.discipline.name }}
+                          br
+                          |  {{ selectDay.homework }}
+                        div {{ selectDay.date }}
+
+          v-col(cols='4')
+            v-card(width='600' flat='')
+              v-card-title
+                p.ml-3
+                  | {{selectDay.teacher.fio}}
+              v-card-text
+                .font-weight-bold.ml-8.mb-2
+                  | Предметы
+                v-timeline(align-top='' dense='')
+                  //v-timeline-item(v-for='discipline in selectDay.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
+                  v-timeline-item(v-for='discipline in selectDay.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
+                    div
+                      div.font-weight-normal
+                        strong {{ selectDay.discipline.name }}
+                        br
+                        |  {{ selectDay.homework }}
+                      div {{ selectDay.date }}
+
+          v-col(cols='4')
+            v-card(width='600' flat='')
+              v-card-title
+                p.ml-3
+                  | {{selectDay.teacher.fio}}
+              v-card-text
+                .font-weight-bold.ml-8.mb-2
+                  | Предметы
+                v-timeline(align-top='' dense='')
+                  //v-timeline-item(v-for='discipline in selectDay.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
+                  v-timeline-item(v-for='discipline in selectDay.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
+                    div
+                      div.font-weight-normal
+                        strong {{ selectDay.discipline.name }}
+                        br
+                        |  {{ selectDay.homework }}
+                      div {{ selectDay.date }}
 
 </template>
 
@@ -17,11 +72,40 @@
             lessons: [],
             lessonDates: [],
             date: new Date().toISOString().substr(0, 10),
+            selectDay: null,
 
+            messages: [
+                {
+                    from: 'You',
+                    message: `Sure, I'll see you later.`,
+                    time: '10:42am',
+                    color: 'deep-purple lighten-1',
+                },
+                {
+                    from: 'John Doe',
+                    message: 'Yeah, sure. Does 1:00pm work?',
+                    time: '10:37am',
+                    color: 'green',
+                },
+                {
+                    from: 'You',
+                    message: 'Did you still want to grab lunch today?',
+                    time: '9:47am',
+                    color: 'deep-purple lighten-1',
+                },
+            ],
         }),
         methods: {
             test(){
-              console.log(this.lessonDates);
+              console.log(this.date);
+            },
+            selectedDay(){
+              this.lessons.forEach((element) => {
+                  if (this.date === element.date) {
+                      this.selectDay = element;
+                      console.log(this.selectDay);
+                  }
+              })
             },
             getAllLessons(){
                 Api.getAllLessons().then(value => {
@@ -40,7 +124,7 @@
             //allowedDates: val => this.lessonDates.indexOf(val) !== -1,
             getAllowedDates (val) {
                 //console.log(JSON.stringify(val));
-                console.log(val);
+                //console.log(val);
                 if (this.lessonDates.indexOf(val) !== -1) {
                     return true
                 } else {
