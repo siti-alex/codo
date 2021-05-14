c31f74c7ecd0887e2f78f16a2c2f95a8f53d117c
lehajuk <asitikovdo@mail.com>
2021-05-14 11:08:43 +0900

    add: journal disciplines done

--- src/components/APersonalSystem/AJournal/index.vue
+++ src/components/APersonalSystem/AJournal/index.vue
@@ -1,29 +1,25 @@
 <template lang="pug">
   div
-    v-btn(@click="test") test
+    //v-btn(@click="test") test
     v-row(justify='center').mt-6
       br
       v-date-picker.mt-4(v-model='date' @input="selectedDay" locale="ru-ru" :allowed-dates='getAllowedDates' full-width='' landscape='' color='#8b2639').ma-5
       v-divider
       v-container(v-if="selectDay.length !==  0" )
-        v-row()
-          v-col(cols='4')
-            v-card(width='600' flat='' v-for="lesson in selectDay" :key="lesson.id")
-              v-card-title
-                p.ml-3
-                  | {{lesson.teacher.fio}}
+            //v-card(width='600' flat='' v-for="lesson in selectDay" :key="lesson.id")
+            v-card(width='600' flat='')
               v-card-text
                 .font-weight-bold.ml-8.mb-2
                   | Предметы
                 v-timeline(align-top='' dense='')
                   //v-timeline-item(v-for='discipline in selectDay.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
-                  v-timeline-item(v-for='discipline in lesson.discipline' :key='discipline.id' :color='discipline.colorCode' small='')
+                  v-timeline-item(v-for='lesson in selectDay' :key='lesson.id' :color='lesson.discipline.colorCode' small='')
                     div
                       div.font-weight-normal
-                        strong {{ discipline.name }}
+                        strong {{ lesson.discipline.name }}
                         br
-                        |  {{ lesson.homework }}
-                      div {{ lesson.date }}
+                        div.font-weight-bold {{ lesson.teacher.fio }}
+                      div ДЗ: {{ lesson.homework }}
 
 </template>
 
@@ -49,6 +45,7 @@
                   if (this.date === element.date) {
                       //this.selectDay = element;
                       this.selectDay.push(element);
+                      console.log(element);
                   }
               })
             },
